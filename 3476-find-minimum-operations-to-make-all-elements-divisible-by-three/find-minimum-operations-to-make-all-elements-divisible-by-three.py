74ac2a6db6a62fd95fd04e013fb48b6404449c0c
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-06 18:34:11 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (9.70%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0 
+        for num in nums:
+            if num % 3 != 0:
+                ans += 1 
+        return ans   
