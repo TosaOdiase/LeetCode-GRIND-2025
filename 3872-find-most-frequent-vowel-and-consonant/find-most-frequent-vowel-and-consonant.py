0908e43956f9ae0ce57b0a17f6030655f93d7b17
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-06 02:39:44 -0400

    Time: 7 ms (45.69%) | Memory: 12.5 MB (11.29%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,25 +1,26 @@
-from collections import Counter 
-
 class Solution(object):
     def maxFreqSum(self, s):
         """
         :type s: str
         :rtype: int
         """
-        # create a set of vowels
+
         vowels = set('aeiou')
-        # get a list of nouns and their frequency 
-        freq = Counter(s)
-        max_v = 0
-        max_c = 0 
+        v = {}
+        c = {}
 
-        for i, k in freq.items():
-            if i in vowels:
-                if k > max_v:
-                    max_v = k
+        for ch in s:
+            if ch in vowels:
+                v[ch] = v.get(ch,0) + 1
             else:
-                if k > max_c:
-                    max_c = k
-        return max_c + max_v
+                c[ch] = c.get(ch,0) + 1
+        
+        maxv = max(v.values()) if v else 0
+        maxc = max(c.values()) if c else 0
+        return maxv + maxc
+
+        
 
 
+
+        
