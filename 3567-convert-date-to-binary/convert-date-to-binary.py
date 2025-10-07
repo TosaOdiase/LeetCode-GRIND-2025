e179e819a100c0871abbe813d5a897862f73061f
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-06 20:42:55 -0400

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.28%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.py
+++ 3567-convert-date-to-binary/convert-date-to-binary.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type date: str
         :rtype: str
         """
-        y,m,d = date.split("-")
-        y = bin(int(y))[2:]
-        m = bin (int(m))[2:]
-        d = bin(int(d))[2:]
-        return '-'.join([y,m,d])
+        new = date.split('-')
+        ans = []
+        for word in new:
+            ans.append(bin(int(word))[2:])
+        return '-'.join(ans)
