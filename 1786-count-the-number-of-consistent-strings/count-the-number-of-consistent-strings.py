6d3469aa07378f06889d97a01553695194d2489f
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-06 21:14:56 -0400

    Time: 201 ms (29.01%) | Memory: 14.7 MB (28.57%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -5,18 +5,10 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        rule = set(allowed)
+        rules = set(allowed)
         ans = 0 
-        goodWord = False
         for word in words:
-            for char in set(word):
-                if char in rule:
-                    goodWord = True
-                else: 
-                    goodWord = False
-                    break
-            if goodWord == True:
-                ans+= 1 
+                if all(char in rules for char in word):
+                    ans+= 1 
         return ans
-
         
