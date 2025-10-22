ae998449cfd57c2a1a4e5454394084746164b429
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-22 16:19:53 -0400

    Time: 27 ms (25.52%) | Memory: 12.6 MB (67.98%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        freq_t = Counter(t)
+        freq_s = Counter(s)
+
+        if freq_t == freq_s:
+            return True 
+        else:
+            return False
