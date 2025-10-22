d2e110b47fc88edcb2886e81bae3e9b78eaad642
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-22 18:15:31 -0400

    Time: 87 ms (51.36%) | Memory: 19.1 MB (71.98%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,18 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # track our buy day and our sell day 
-        # our buy day must be before our sell day 
-        # need to buy at the minmum, sell at the max
-
-        l = 0 
-        r = 1
-        maxProfit = 0 
-
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                maxProfit = max(maxProfit, prices[r] - prices[l])
+        minP = float('inf')
+        maxP = 0 
+        for price in prices:
+            if price < minP:
+                minP = price 
             else:
-                l = r 
-            r += 1
-        return maxProfit
+                maxP = max(maxP, price - minP)
+        return maxP
