562255971cc5ce454d0d8752b7f3390e2efcdec5
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-22 19:53:39 -0400

    Time: 11 ms (56.13%) | Memory: 13.5 MB (74.03%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -7,15 +7,11 @@ class Solution(object):
         """
         mapAB, mapBA = {}, {}
 
-        for i in range (len(s)):
+        for i in range(len(s)):
             c1, c2 = s[i], t[i]
-            if (c1 in mapAB and mapAB[c1] != c2) or (c2 in mapBA and mapBA[c2] != c1):
+
+            if mapAB.get(c1) != mapBA.get(c2):
                 return False
-            mapAB[c1] = c2
-            mapBA[c2] = c1
+            mapAB[c1] = i
+            mapBA[c2] = i
         return True
-
-
-
-
-        
