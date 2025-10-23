5426c6804868691f4aa9796302d4603bb7b53fb9
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-22 23:47:15 -0400

    Time: 3 ms (32.26%) | Memory: 12.3 MB (92.36%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -6,13 +6,10 @@ class Solution(object):
         """
         res = ""
         for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
+            for word in strs:
+                if i == len(word) or word[i] != strs[0][i]:
                     return res
             res += strs[0][i]
         return res
-
-
-
-
         
+            
