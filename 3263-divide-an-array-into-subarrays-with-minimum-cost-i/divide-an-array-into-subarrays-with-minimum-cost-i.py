f8cd8dd3f9e6af8b821bd9b766eb205b270c4cc1
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-23 00:28:38 -0400

    Time: 3 ms (35.14%) | Memory: 12.3 MB (86.49%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -4,23 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # must have three subarray 
-        # must find the minimum
-        # first number in subarray is the value 
-
-        # after the first, must find the two lowest numbers to start at 
-
-        # create a best value 
-
-        #exclude the first index 
+        # need the array to split into three 
+        # get the first index to be a subarray 
+        # find the next two lowest indexes and add them to total 
+        x = nums[0]
         n = len(nums)
-        d = nums[0]
-
-        # sort the rest, to get the two lowest in front 
-
         nums[1:n] = sorted(nums[1:n])
-
-        ans = d + nums[1] + nums[2]
-        return ans
-
-        
+        return x + nums[1] + nums[2]
