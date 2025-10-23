96eae04e303bd53d7eb7fc191258ba5ed8989414
Tosa Odiase <140124217+TosaOdiase@users.noreply.github.com>
2025-10-23 00:15:14 -0400

    Time: 21 ms (60.66%) | Memory: 14.4 MB (14.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,20 +5,14 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
-        # create a dictionary, with characters and their freqs 
-        # check if lengths are equal 
-
         if len(s) != len(t):
             return False 
-        
-        count_t, count_s = {}, {}
-
+        countS, countT = {}, {}
         for i in range(len(s)):
-            count_s[s[i]] = 1 + count_s.get(s[i], 0)
-            count_t[t[i]] = 1 + count_t.get(t[i], 0)
-        for c in count_t:
-            if count_t[c] != count_s.get(c, 0):
-                return False
+            countT[s[i]] = 1 + countT.get(s[i], 0)
+            countS[t[i]] = 1 + countS.get(t[i], 0)
+        for num in countT:
+            if countT.get(num, 0) != countS.get(num, 0):
+                return False 
         return True
         
